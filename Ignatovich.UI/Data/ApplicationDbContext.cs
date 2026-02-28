68bb98e8bfebf2e51b2da4e6e8cc89b4502d4ff6
Dmitry <isyzes@gmail.com>
2026-02-28 12:44:45 +0300

    Add lab 1

---
+++ Ignatovich.UI/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ignatovich.UI.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
