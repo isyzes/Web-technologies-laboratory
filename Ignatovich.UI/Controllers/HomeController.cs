9097c131aed383fb8f5b47128f2faef0bfbd21a8
Dmitry <isyzes@gmail.com>
2026-05-31 17:53:36 +0300

    Add error handler

--- Ignatovich.UI/Controllers/HomeController.cs
+++ Ignatovich.UI/Controllers/HomeController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using Ignatovich.UI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Ignatovich.UI.Controllers
+namespace Ignatovich.UI.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    public IActionResult Index()
     {
-        public IActionResult Index()
+        return View();
+    }
+
+    [AllowAnonymous]
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel
         {
-            return View();
-        }
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+        });
     }
 }
